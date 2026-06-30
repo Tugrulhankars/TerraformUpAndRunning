7e8a402a80e8c65daed92afc00c92c6331b84504
Tugrulhan Karsli <karslitugrulhan@gmail.com>
2026-06-30 21:14:14 +0300

    image_id updated

--- main.tf
+++ main.tf
@@ -127,8 +127,8 @@ resource "aws_lb_listener_rule" "asg" {
 
 resource "aws_launch_configuration" "example" {
 
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = "t2.micro"
+  image_id        = "ami-0aba19e56f3eaec05"
+  instance_type   = "t3.micro"
   security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
